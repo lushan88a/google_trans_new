472618e1b44b5d8fc3018820521071e566f2fe43
lushan88a <54923572+lushan88a@users.noreply.github.com>
2020-11-20 20:05:59 +0800

    Update google_trans_new.py

--- google_trans_new.py
+++ google_trans_new.py
@@ -5,7 +5,7 @@ from urllib.parse import quote
 from six.moves import urllib
 import urllib3
 import logging
-from .constant import LANGUAGES,DEFAULT_SERVICE_URLS
+from constant import LANGUAGES,DEFAULT_SERVICE_URLS
 
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
@@ -57,10 +57,9 @@ class google_new_transError(Exception):
 class google_translator:
     '''
     You can use 108 language in target and source,details view LANGUAGES.
-    Target language: like 'en'、'zh'.'th'...
+    Target language: like 'en'、'zh'、'th'...
     '''
-
-    def __init__(self, url_suffix="cn"):
+    def __init__(self,url_suffix="cn"):
         if url_suffix not in URLS_SUFFIX:
             self.url_suffix = URL_SUFFIX_DEFAULT
         else:
@@ -68,9 +67,9 @@ class google_translator:
         url_base = "https://translate.google.{}".format(self.url_suffix)
         self.url = url_base + "/_/TranslateWebserverUi/data/batchexecute"
 
-    def _package_rpc(self, text, lang_src='auto', lang_tgt='auto'):
+    def _package_rpc(self,text,lang_src='auto',lang_tgt='auto'):
         GOOGLE_TTS_RPC = ["MkEWBc"]
-        parameter = [[text.strip(), lang_src, lang_tgt, True], [1]]
+        parameter = [[text.strip(), lang_src, lang_tgt, True],[1]]
         escaped_parameter = json.dumps(parameter, separators=(',', ':'))
         rpc = [[[random.choice(GOOGLE_TTS_RPC), escaped_parameter, None, "generic"]]]
         espaced_rpc = json.dumps(rpc, separators=(',', ':'))
@@ -79,22 +78,24 @@ class google_translator:
         freq = freq_initial
         return freq
 
-    def translate(self, text, lang_tgt='auto', lang_src='auto'):
+    def translate(self,text,lang_tgt='auto',lang_src='auto'):
         try:
             lang = LANGUAGES[lang_src]
-        except:
+        except :
             lang_src = 'auto'
         try:
             lang = LANGUAGES[lang_tgt]
-        except:
+        except :
             lang_src = 'auto'
-        if lang_src == '':
+        if lang_src == '' :
             lang_src = "auto"
         if lang_tgt == '':
             lang_tgt = 'auto'
         lang_src = lang_src
         lang_tgt = lang_tgt
-        text = text.strip('\n').replace('\n', "").replace('\t', '')
+        text = text.strip('\n').replace('\n',"").replace('\t','')
+        if len(text) >= 5000:
+            return "Warning: Can only detect less than 5000 characters"
         if len(text) == 0:
             return ""
         headers = {
@@ -105,11 +106,11 @@ class google_translator:
                 "Chrome/47.0.2526.106 Safari/537.36",
             "Content-Type": "application/x-www-form-urlencoded;charset=utf-8"
         }
-        freq = self._package_rpc(text, lang_src, lang_tgt)
+        freq = self._package_rpc(text,lang_src,lang_tgt)
         response = requests.Request(method='POST',
-                                    url=self.url,
-                                    data=freq,
-                                    headers=headers)
+                                     url=self.url,
+                                     data=freq,
+                                     headers=headers)
         try:
             with requests.Session() as s:
                 r = s.send(request=response.prepare(),
@@ -119,14 +120,15 @@ class google_translator:
             for line in r.iter_lines(chunk_size=1024):
                 decoded_line = line.decode('utf-8')
                 if "MkEWBc" in decoded_line:
-                    if lang_src == 'auto' or lang_tgt == 'auto':
-                        data_got = re.search(r',null,\[\[\\\"(.*?)\\\"\]', decoded_line).group(1)
-                        data_got = data_got.replace('\\\\\\', "").split('\\\",')[0]
+                    try :
+                        sentences = re.findall(r'\[\\\"(.*?)\\\"\]',"[[\\\"" + decoded_line.split(r',[[\"')[-1])
+                        data_got = ""
+                        for sentence in sentences :
+                            sentence = sentence.replace('\\\\\\','')
+                            data_got += sentence.split(r"\",[\"")[0].strip() + ' '
                         return data_got
-                    else:
-                        data_got = re.search(r',null,\[\[\\\"(.*?)\\\",\[', decoded_line).group(1)
-                        data_got = data_got.replace('\\\\\\', "")
-                        return data_got
+                    except :
+                        return "ERROR"
             r.raise_for_status()
         except requests.exceptions.HTTPError as e:
             # Request successful, bad response
@@ -137,8 +139,10 @@ class google_translator:
             log.debug(str(e))
             raise google_new_transError(tts=self)
 
-    def detect(self, text):
-        text = text.strip('\n').replace('\n', "").replace('\t', '')
+    def detect(self,text):
+        text = text.strip('\n').replace('\n',"").replace('\t','')
+        if len(text) >= 5000:
+            return log.debug("Warning: Can only detect less than 5000 characters")
         if len(text) == 0:
             return ""
         headers = {
@@ -151,9 +155,9 @@ class google_translator:
         }
         freq = self._package_rpc(text)
         response = requests.Request(method='POST',
-                                    url=self.url,
-                                    data=freq,
-                                    headers=headers)
+                                     url=self.url,
+                                     data=freq,
+                                     headers=headers)
         try:
             with requests.Session() as s:
                 r = s.send(request=response.prepare(),
@@ -163,13 +167,15 @@ class google_translator:
             for line in r.iter_lines(chunk_size=1024):
                 decoded_line = line.decode('utf-8')
                 if "MkEWBc" in decoded_line:
+                    regex_str = r"\[\[\"wrb.fr\",\"MkEWBc\",\"\[\[(.*?)\\\",\[\[\[|\[\[\"wrb.fr\",\"MkEWBc\",\"\[\[(.*?)\\\"\\n,\[\[\["
                     try:
-                        data_got = re.search(r',null,\\\"(.*?)\\\",\[\[\[', decoded_line).group(1)
+                        data_got = re.search(regex_str,decoded_line).group(1)
                     except:
-                        return "error"
-                    data_got = data_got.replace('\\\\\\', "")
+                        return 'ERROR'
+                    data_got = data_got.split('\\"')[-1]
+                    data_got = data_got.replace('\\\\\\',"")
                     data_got = data_got.split('\\\"]')[0]
-                    return [data_got, LANGUAGES[data_got.lower()]]
+                    return [data_got,LANGUAGES[data_got.lower()]]
             r.raise_for_status()
         except requests.exceptions.HTTPError as e:
             # Request successful, bad response
